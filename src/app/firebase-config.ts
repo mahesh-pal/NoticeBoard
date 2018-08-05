fd3165305aa6a330db8d790f3099e21a5f751920
Mahesh Pal <rathaur.m7@gmail.com>
2018-08-05 11:38:49 +0530

    d

--- src/app/firebase-config.ts
+++
@@ -1,8 +0,0 @@
-export const  config = {
-  apiKey: "AIzaSyDt3_olyP-GoVKf2vPNI9DNyznreWxoPNQ",
-  authDomain: "noticeboard-6458d.firebaseapp.com",
-  databaseURL: "https://noticeboard-6458d.firebaseio.com",
-  projectId: "noticeboard-6458d",
-  storageBucket: "noticeboard-6458d.appspot.com",
-  messagingSenderId: "151751240535"
-};
